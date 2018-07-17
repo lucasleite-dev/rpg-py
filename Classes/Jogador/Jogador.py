3244882f50727e3e2bebc8ca49712a16e2e346ff
RaolootneXII <jv20252025@gmail.com>
2018-07-17 15:38:50 -0300

    Inventário, estilo de menu e limpando um pouco o código

--- Classes/Jogador/Jogador.py
+++ Classes/Jogador/Jogador.py
@@ -7,11 +7,11 @@ class Jogador:
     def __init__(self, vida_max=100, vida=100, mana=100, ataque=15,defesa=50, status="Vivo", name=""):
         self.vida_max = vida_max
         self.vida = vida
-        self.mana = mana 
+        self.mana = mana
         self.ataque = ataque
         self.defesa = defesa
         self.status = status
-        self.inventario = {}
+        self.inventario = {"Poção de Mana": 2, "Poção de Vida": 3}
         self.name = name
 
     def levaDano(self, dano):
@@ -20,3 +20,29 @@ class Jogador:
             self.vida = 0
             self.status = "Morto"
         return int(dano / (1+self.defesa/100))
+
+    def getInventario(self): # Função para retornar os itens do inventário
+        print("+----------------------------------+")
+        print("|            Inventário            |")
+        print("V----------------------------------V")
+        if self.inventario == {}:
+            print("| [Vazio]                          |")
+        else:
+            for key in self.inventario:
+                resto = (30-len(key))-len(str(self.inventario[key]))
+                print(f"| {key} : {self.inventario[key]}" + " "*resto + "|")
+        print("+----------------------------------+")
+        print("| Pressione enter para voltar      |")
+        print("^----------------//----------------^")
+        e = input("")
+
+    # Mostra o status do jogador, nome;hp;mp;etc;
+    def getStatus(self):
+        teste = str(self.vida)
+        print("+----------------------------------+")
+        print("|              Status              |")
+        print("V----------------------------------V")
+        print(f"| Nome: {self.name}" + " "*(27-len(self.name)) + "|")
+        print(f"| HP  : {self.vida}" + " "*(27-len(str(self.vida))) + "|")
+        print(f"| MP  : {self.mana}" + " "*(27-len(str(self.mana))) + "|")
+        print("^----------------//----------------^")
--- Main.py
+++ Main.py
@@ -13,39 +13,38 @@ def limparTela():
     sistema = platform.system()
     if sistema == "Windows":
         os.system("cls")
-
-#Mostra o status do jogador, nome;hp;mp;etc;
-def statusJogador():
-    print(7*"-")
-    print("STATUS")
-    print(7*"-")
-    print(f"Nome: {jogador.name}")
-    print(f"HP: {jogador.vida}")
-    print(f"MP: {jogador.mana}")
-    print(7*"-")
+    else:
+        os.system("clear")
 
 #Mostra o status do inimigo, nome;hp;mp;etc;
 def statusInimigo():
-    print(f"STATUS INIMIGO")
-    print(7*"-")
-    print(f"Nome: {rato.name}")
-    print(f"HP: {rato.vida}")
-    print(f"Estado: {rato.status}")
-    print(7*"-")
-    
-#Mostra o combat log do ataque fraco
-def combatLog():
-    print("COMBAT LOG:")
-    print(f"{jogador.name} atacou e causou {rato.levaDano(jogador.ataque)} de dano.")
-    print(f"{jogador.name} recebeu {jogador.levaDano(rato.ataque)} de dano.")
-    print(30*"-")
+    print("+----------------------------------+")
+    print("|           Status Inimgo          |")
+    print("V----------------------------------V")
+    print(f"| Nome  : {rato.name}" + " "*(25-len(rato.name)) + "|")
+    print(f"| HP    : {rato.vida}" + " "*(25-len(str(rato.vida))) + "|")
+    print(f"| Estado: {rato.status}" + " "*(25-len(rato.status)) + "|")
+    print("^----------------//----------------^")
 
-#Mostra o combat log do ataque forte
-def combatLog2():
-    print("COMBAT LOG:")
-    print(f"{jogador.name} atacou e causou {rato.levaDano(jogador.ataque)* 2} de dano.")
-    print(f"{jogador.name} recebeu {jogador.levaDano(rato.ataque)} de dano.")
-    print(30*"-")
+#Mostra o combat log do ataque fraco
+def combatLog(forca=1):
+    print("+----------------------------------+")
+    print("|            Combat Log            |")
+    print("V----------------------------------V")
+    print(f"| {jogador.name} atacou e causou {rato.levaDano(jogador.ataque)*forca} de dano.")
+    print(f"| {jogador.name} recebeu {jogador.levaDano(rato.ataque)} de dano.")
+    print("^----------------//----------------^")
+
+def loading(mensagem, tempo):
+    c = 0
+    while c < len(mensagem):
+        if mensagem[c] != '.':
+            print(mensagem[c], end='', flush=True)
+        else:
+            sleep(tempo)
+            print(mensagem[c], end='', flush=True)
+        c += 1
+    print("\n")
 
 #Váriaveis
 jogador = Jogador()
@@ -53,102 +52,97 @@ rato = Rato()
 fim_de_jogo = False
 jogador.name = input("Nome: ")
 limparTela()
+explorar = ""
 
 while True:
-    statusJogador()
-    print("MENU")
-    print(7*"-")
-    print("1 - Explorar 2 - Inventário 3 - Sair do Jogo")
-    print("")
-    #Menu
-    escolha = input()
+    jogador.getStatus()
+    print("+----------------------------------+")
+    print("|               Menu               |")
+    print("V----------------------------------V")
+    print("| 1 - Explorar                     |")
+    print("| 2 - Inventário                   |")
+    print("| 3 - Sair do Jogo                 |")
+    print("^----------------//----------------^")
+    escolha = input("| ?: ")
     limparTela()
     if escolha == "1": #Carregamento de tela da Exploração
-        statusJogador()
-        print("Explorando", end="", flush=True)
-        sleep(0.5)
-        print(".", end="", flush=True)
-        sleep(0.5)
-        print(".", end="", flush=True)
-        sleep(0.5)
-        print(".")
+        jogador.getStatus()
+        loading("Explorando...", 0.5)
         explorar = randint(1, 5) #gera um número aleátorio, se for 1,2,3 = fight com rato
+        if explorar == 1 or explorar == 2 or explorar == 3: #Tela de combate
+            limparTela()
+            jogador.getStatus()
+            statusInimigo()
+            print(f"Entrou em combate com um {rato.name}!")
+            print(30*"-")
+            while rato.status == "Vivo":
+                print("1 - Ataque Normal, Custo: 0 MP\n2 - Ataque Forte, Custo: 30 MP\n3 - Para fugir.") #Menu de Ataques
+                ataques = input()
+                print(5*"=")
+                #Ataque normal
+                if ataques == "1": #Ataques
+                    limparTela()
+                    #Quando o inimigo recebe dano
+                    rato.levaDano(jogador.ataque)
+                    #Quando o jogador recebe dano
+                    jogador.levaDano(rato.ataque)
+                    #Verifica a vida do jogador
+                    if jogador.vida > 0:
+                        jogador.getStatus()
+                    #Verifica se a vida do inimigo é maior que 0
+                    if rato.vida > 0:
+                        statusInimigo()
+                    #Combate log
+                    combatLog()
+                #Quando jogador usa ataque forte
+                if ataques == "2":
+                    limparTela()
+                    rato.levaDano(jogador.ataque) * 2
+                    jogador.levaDano(rato.ataque*100)
+                    jogador.mana -= 30
+                    #Verifica a vida do jogador
+                    if jogador.vida > 0:
+                        jogador.getStatus()
+                    #Verifica se a vida do inimigo é maior que 0
+                    if rato.vida > 0:
+                        statusInimigo()
+                    combatLog(2)
+                #Quando o jogador foge
+                if ataques == "3":
+                    limparTela()
+                    print("Você fugiu do combate.")
+                    jogador.vida = 100
+                    jogador.mana = 100
+                    break
+                #Quando o jogador morre.
+                if jogador.vida <=0:
+                    print(f"{jogador.name} morreu!\nGAMER OVER")
+                    fim_de_jogo = True
+                    break
+                #Quando inimigo morre.
+                if rato.vida <=0:
+                    statusInimigo()
+                    #print("O rato dropou 1 Poção")
+                    #jogador.inventario += rato.drop
+                    jogador.vida = 100
+                    jogador.mana = 100
+                    sleep(0.5)
+                    limparTela()
+        else:
+            print("Nada foi encontrado.")
+            sleep(1.5)
+            limparTela()
+        if rato.vida <= 0 and rato.status == "Morto":
+            rato.vida = 50
+            rato.status = "Vivo"
     if escolha == "2": #Carregamento da tela de inventario
-        print("Carregando inventário", end="", flush=True)
-        sleep(0.5)
-        print(".", end="", flush=True)
-        sleep(0.5)
-        print(".", end="", flush=True)
-        sleep(0.5)
-        print(".")
-        print(jogador.inventario)
+        limparTela()
+        loading("\nCarregando inventário...", 0.5)
+        limparTela()
+        jogador.getInventario()
+        limparTela()
     if escolha == "3" or fim_de_jogo == True:
         break
-    if explorar == 1 or explorar == 2 or explorar == 3: #Tela de combate
-        limparTela()
-        statusJogador()
-        statusInimigo()
-        print(f"Entrou em combate com um {rato.name}!")
-        print(30*"-")
-        while rato.status == "Vivo":
-            print("1 - Ataque Normal, Custo: 0 MP\n2 - Ataque Forte, Custo: 30 MP\n3 - Para fugir.") #Menu de Ataques
-            ataques = input()
-            print(5*"=")
-            #Ataque normal
-            if ataques == "1": #Ataques
-                limparTela()
-                #Quando o inimigo recebe dano
-                rato.levaDano(jogador.ataque)
-                #Quando o jogador recebe dano
-                jogador.levaDano(rato.ataque)
-                #Verifica a vida do jogador
-                if jogador.vida > 0:
-                    statusJogador()
-                #Verifica se a vida do inimigo é maior que 0
-                if rato.vida > 0:
-                    statusInimigo()
-                #Combate log
-                combatLog()
-            #Quando jogador usa ataque forte
-            if ataques == "2":
-                limparTela()
-                rato.levaDano(jogador.ataque) * 2
-                jogador.levaDano(rato.ataque)
-                jogador.mana -= 30
-                #Verifica a vida do jogador
-                if jogador.vida > 0:
-                    statusJogador()
-                #Verifica se a vida do inimigo é maior que 0
-                if rato.vida > 0:
-                    statusInimigo()
-                combatLog2()
-            #Quando o jogador foge
-            if ataques == "3":
-                limparTela()
-                print("Você fugiu do combate.")
-                jogador.vida = 100
-                jogador.mana = 100
-                break
-            #Quando o jogador morre.
-            if jogador.vida <=0:
-                print(f"{jogador.name} morreu!\nGAMER OVER")
-                fim_de_jogo = True
-                break
-            #Quando inimigo morre.
-            if rato.vida <=0:
-                statusInimigo()
-                #print("O rato dropou 1 Poção")
-                #jogador.inventario += rato.drop
-                jogador.vida = 100
-                jogador.mana = 100
-                limparTela()
-    else:
-        print("Nada foi encontrado.")
-        sleep(1.5)
-        limparTela()
-    if rato.vida <= 0 and rato.status == "Morto":
-        rato.vida = 50
-        rato.status = "Vivo"
-    
+
 print("Você saiu do Jogo!")
 sleep(2)
